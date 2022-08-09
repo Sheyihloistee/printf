4a4bf88acb8a735fcfa289d03116a8f0c9c88ad9
Nwachukwu Michael .C <106752190+Chicajo@users.noreply.github.com>
2022-08-09 13:58:37 +0100

    Update check_flags.c

--- check_flags.c
+++ check_flags.c
@@ -36,10 +36,10 @@ int get_flag(char s, flags_t *f)
 			f->space = 1;
 			i = 1;
 			break;
-		 case '0':
-                        f->space = 1;
-                        i = 1;
-                        break
+		case '0':
+			f->space = 1;
+			i = 1;
+			break
 	}
 
 	return (i);
